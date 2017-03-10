b4190724b108460632fde621cddaaef1e6545226
Graham Fairweather <graham.fairweather@ispy.se>
2017-03-10 08:34:22 +0100

    Use yarn, update dependencies, add latest node versions, cleanup

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 <a name="module_is-object-like-x"></a>
+
 ## is-object-like-x
 <a href="https://travis-ci.org/Xotic750/is-object-like-x"
 title="Travis status">
@@ -39,11 +40,12 @@ es5-shim.js to be able to work properly.
 `es6.shim.js` provides compatibility shims so that legacy JavaScript engines
 behave as closely as possible to ECMAScript 6 (Harmony).
 
-**Version**: 1.0.11  
-**Author:** Xotic750 <Xotic750@gmail.com>  
+**Version**: 1.1.0  
+**Author**: Xotic750 <Xotic750@gmail.com>  
 **License**: [MIT](&lt;https://opensource.org/licenses/MIT&gt;)  
 **Copyright**: Xotic750  
 <a name="exp_module_is-object-like-x--module.exports"></a>
+
 ### `module.exports(value)` ⇒ <code>boolean</code> ⏏
 Checks if `value` is object-like. A value is object-like if it's not a
 primitive and not a function.
--- tests/spec/test.js
+++ tests/spec/test.js
@@ -1,18 +1,22 @@
-/*jslint maxlen:80, es6:true, white:true */
+/* jslint maxlen:80, es6:true, white:true */
 
-/*jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
-  freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
-  nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-  es3:false, esnext:true, plusplus:true, maxparams:2, maxdepth:2,
-  maxstatements:11, maxcomplexity:3 */
+/* jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
+   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
+   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
+   es3:true, esnext:true, plusplus:true, maxparams:1, maxdepth:2,
+   maxstatements:11, maxcomplexity:3 */
 
-/*global JSON:true, expect, module, require, describe, it, returnExports */
+/* eslint strict: 1, max-lines: 1, symbol-description: 1, max-nested-callbacks: 1,
+   max-statements: 1, array-callback-return: 1, no-sparse-arrays: 1 */
 
-(function () {
+/* global JSON:true, expect, module, require, describe, it, returnExports */
+
+;(function () { // eslint-disable-line no-extra-semi
+
   'use strict';
 
-  var hasSymbol = typeof Symbol === 'function' && typeof Symbol() === 'symbol',
-    isObjectLike;
+  var hasSymbol = typeof Symbol === 'function' && typeof Symbol() === 'symbol';
+  var isObjectLike;
   if (typeof module === 'object' && module.exports) {
     require('es5-shim');
     require('es5-shim/es5-sham');
@@ -33,21 +37,19 @@
       expect(isObjectLike(Object(false))).toBe(true);
       expect(isObjectLike(new Date())).toBe(true);
       expect(isObjectLike(new Error())).toBe(true);
-      expect(isObjectLike({
-        'a': 1
-      })).toBe(true);
+      expect(isObjectLike({ a: 1 })).toBe(true);
       expect(isObjectLike(Object(0))).toBe(true);
       expect(isObjectLike(/x/)).toBe(true);
       expect(isObjectLike(Object('a'))).toBe(true);
     });
 
     it('should return `false` for non-objects', function () {
-      var symbol = hasSymbol && Symbol(),
-        /*jshint elision:true*/
-        values = [, '', 0, false, NaN, null, undefined, true, 1, 'a', symbol],
-        expected = values.map(function () {
-          return false;
-        });
+      var symbol = hasSymbol && Symbol();
+      /* jshint elision:true*/
+      var values = [, '', 0, false, NaN, null, undefined, true, 1, 'a', symbol];
+      var expected = values.map(function () {
+        return false;
+      });
 
       var actual = values.map(function (value, index) {
         return index ? isObjectLike(value) : isObjectLike();
