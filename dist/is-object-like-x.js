aebc69552366e5a716ae1410398339eab18d8437
Graham Fairweather <xotic750@gmail.com>
2019-07-27 02:57:40 +0200

    :bookmark: v2.0.24
    Auto build

--- dist/is-object-like-x.js
+++ dist/is-object-like-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-26T13:14:20.957Z",
+  "date": "2019-07-27T00:57:34.735Z",
   "describe": "",
   "description": "Determine if a value is object like.",
   "file": "is-object-like-x.js",
   "hash": "dde8e02f223aa505c876",
   "license": "MIT",
-  "version": "2.0.23"
+  "version": "2.0.24"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
