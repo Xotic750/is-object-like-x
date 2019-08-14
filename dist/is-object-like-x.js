d6ca084db3d2aae04526447c2cd3ef9a1fbef2c4
Graham Fairweather <xotic750@gmail.com>
2019-08-14 16:00:19 +0200

    :bookmark: v2.0.26
    Auto build

--- dist/is-object-like-x.js
+++ dist/is-object-like-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-08-13T13:05:49.816Z",
+  "date": "2019-08-14T14:00:13.850Z",
   "describe": "",
   "description": "Determine if a value is object like.",
   "file": "is-object-like-x.js",
